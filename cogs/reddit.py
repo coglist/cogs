680ac9a032c376f6771b524a8c07aaf652b0b93b
elf <49889930+elfq@users.noreply.github.com>
2021-02-10 17:28:40 -0500

    fixed nsfw check bug

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -15,15 +15,14 @@ class RedditCog(commands.Cog):
             res = await r.json()
             random_post = random.randint(1, 25)
             
-            embed = discord.Embed(description = res['data']['children'][random_post]['data']['selftext'],color = discord.Colour.red())
-            
-            embed.set_author(name=res['data']['children'][random_post]['data']['title'], url=f"https://reddit.com{res['data']['children'][random_post]['data']['permalink']}")
-            
             if res["data"]["children"][random_post]['data']["over_18"] == True:
               res["data"]["children"][random_post]['data']["url"] = "https://media4.giphy.com/media/10kABVanhwykJW/giphy.gif"
               res["data"]["children"][random_post]['data']["title"] = "This post is marked as NSFW, therefore it will not be displayed, sorry!"
               res["data"]["children"][random_post]['data']["permalink"] = "/r/rickroll/"
             
+            embed = discord.Embed(description = res['data']['children'][random_post]['data']['selftext'],color = discord.Colour.red())
+
+            embed.set_author(name=res['data']['children'][random_post]['data']['title'], url=f"https://reddit.com{res['data']['children'][random_post]['data']['permalink'
             embed.set_image(url=res["data"]["children"][random_post]['data']["url"])
             embed.set_footer(text=f"👍 {res['data']['children'][random_post]['data']['ups']} 💬 {res['data']['children'][random_post]['data']['num_comments']}")
             await ctx.send(embed=embed)
